539c81e69e0d864ab9d0b9ab0013ff13fa8710f7
Dark Knight <jordanccall@gmail.com>
2020-07-28 19:32:59 -0600

    fixed the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,8 @@
 import React from "react";
 
 const Footer = () => (
-  <footer className="bg-light p-3 text-center">
-    <div className="logo" />
-    <p>
-      Sample project provided by <a href="https://auth0.com">Auth0</a>
-    </p>
+  <footer className="footer pt-4 text-center">
+    <p><strong>EvenCuriouser</strong> Copyright © 2020</p>
   </footer>
 );
 
