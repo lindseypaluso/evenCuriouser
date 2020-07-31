c3eca80bd0e3d07bc25d1b4870b679b7207d3084
Dark Knight <jordanccall@gmail.com>
2020-07-30 23:45:31 -0600

    made some changes to the chat and app

--- src/App.js
+++ src/App.js
@@ -34,13 +34,13 @@ const App = () => {
     <Router history={history}>
       <div id="app" className="d-flex flex-column h-100">
         <NavBar />
-        <Container className="flex-grow-1 mt-5">
+        
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/profile" component={Profile} />
             <Route path="/external-api" component={ExternalApi} />
           </Switch>
-        </Container>
+        
         <Footer />
       </div>
     </Router>
