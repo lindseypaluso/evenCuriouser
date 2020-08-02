5065fd66292af667271f11dc47be996e69259f81
lisacampbell <59990057+lisaswindler@users.noreply.github.com>
2020-08-01 19:26:26 -0600

    username

--- client/src/components/UserName.js
+++ client/src/components/UserName.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
 
-import Highlight from "../components/Highlight";
-import Loading from "../components/Loading";
+import Highlight from "./Highlight";
+import Loading from "./Loading";
 import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 
 export const ProfileComponent = () => {
