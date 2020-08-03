3d61c13261abedb41a673f7b82cdbbc3bff71877
Lindsey Paluso <lindzymarie@gmail.com>
2020-08-03 11:52:39 -0700

    test this route

---
+++ client/build/precache-manifest.67ee9a2c5b2869c4a61748db351f1fb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "62edf19d140b5780b941",
+    "url": "/static/css/main.31a7349a.chunk.css"
+  },
+  {
+    "revision": "62edf19d140b5780b941",
+    "url": "/static/js/main.bbb66f4f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3f5bd2b2b026c0c74436",
+    "url": "/static/css/2.a351baf4.chunk.css"
+  },
+  {
+    "revision": "3f5bd2b2b026c0c74436",
+    "url": "/static/js/2.8de79c21.chunk.js"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "c45c60f050a3a07ee9df17b7841296e7",
+    "url": "/index.html"
+  }
+];
